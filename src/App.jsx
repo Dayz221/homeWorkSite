557348152b44de8abb51f3e813119940225731b7
Dayz221 <rutuchenal37@gmail.com>
2024-09-15 00:08:24 +0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 export default () => {
+  const tg = window.Telegram.WebApp
+
   return (
-    <h2>Hello world!!</h2>
+    <>
+      <h2>Hello world!!</h2>
+      { console.log(tg) }
+    </>
   )
 }
