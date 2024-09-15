8bcd6c7a17ed6c92c5593534c36c1c06a9b18c76
Dayz221 <rutuchenal37@gmail.com>
2024-09-15 11:09:13 +0300

    уа

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ export default () => {
 
   return (
     <>
-      <h2>User: {tg.initDataUnsafe.user.id}</h2>
-      <h2>First Name: {tg.initDataUnsafe.user.first_name}</h2>
-      <h2>Username: {tg.initDataUnsafe.user?.username}</h2>
+      {tg ?
+        <>
+          <h2>User: {tg.initDataUnsafe.user.id}</h2>
+          <h2>First Name: {tg.initDataUnsafe.user.first_name}</h2>
+          <h2>Username: {tg.initDataUnsafe.user?.username}</h2>
+        </>
+      :
+      <h2> Данные пользователя не найдены </h2>  
+    }
     </>
   )
 }
