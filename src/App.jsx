041fb1dfda255a6cfe8eb037bf927645b64af6fd
Dayz221 <rutuchenal37@gmail.com>
2024-09-15 10:54:29 +0300

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ export default () => {
 
   return (
     <>
-      <h2>Hello world!!</h2>
-      <div>{tg}</div>
+      <h2>User: {tg.initDataUnsafe.user.id}</h2>
+      <h2>First Name: {tg.initDataUnsafe.user.first_name}</h2>
+      <h2>Photo: {tg.initDataUnsafe.user?.photourl}</h2>
+      <h2>Username: {tg.initDataUnsafe.user?.username}</h2>
     </>
   )
 }
