e7d8640852309fc79842e9a120996ec684aef34a
Dayz221 <rutuchenal37@gmail.com>
2024-09-15 15:07:32 +0300

    sd

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,8 +1,11 @@
 import Button from "../components/Button/Button.jsx"
+import '../styles/register.css'
 
 export default () => {
     return (
-        <div style={{margin: "0 auto", maxWidth: "400px", padding: "10px"}}>
+        <div className="container">
+            <h2 className="mainLable">Регистрация</h2>
+ 
             <Button text="Зарегистрироваться" fullWidth />
         </div>
     )
