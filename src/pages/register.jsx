981b08959087cdcec0a6866ca882ac211d5aa88d
Dayz221 <rutuchenal37@gmail.com>
2024-09-15 19:36:37 +0300

    sd

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,14 @@
 import Button from "../components/Button/Button.jsx"
+import Input from "../components/Input/Input.jsx"
+import PasswordInput from "../components/PasswordInput/PasswordInput.jsx"
 import '../styles/register.css'
 
 export default () => {
     return (
         <div className="container">
             <h2 className="mainLable">Регистрация</h2>
+
+            <PasswordInput />
  
             <Button text="Зарегистрироваться" fullWidth />
         </div>
