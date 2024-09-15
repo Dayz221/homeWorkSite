8464031331c650c28de8028db5fb4b1de1f97964
Dayz221 <rutuchenal37@gmail.com>
2024-09-15 13:17:27 +0300

    вау

--- src/pages/userPage.jsx
+++ src/pages/userPage.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default () => {
     return (
-        <h2> Страница Пользователя </h2>
+        <>
+            <h2> Страница Пользователя </h2>
+            <Link to="/auth/register">Регистрация</Link>
+            <Link to="/auth/login">Вход</Link>
+        </>
     )
 }
