5332c769cce48d9efc6f1c96502f6996115db3e3
Dayz221 <rutuchenal37@gmail.com>
2024-09-15 12:47:22 +0300

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './app.css'
+import './styles/app.css'
 import UserPage from './pages/userPage.jsx'
 import Register from './pages/register.jsx'
 import Login from './pages/login'
