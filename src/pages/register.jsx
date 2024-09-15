308061230d3c8e8173e3322b03952866acfcba4e
Dayz221 <rutuchenal37@gmail.com>
2024-09-15 22:22:55 +0300

    sd

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -3,11 +3,13 @@ import Button from "../components/Button/Button.jsx"
 import Input from "../components/Input/Input.jsx"
 import '../styles/register.css'
 import axios from "axios"
+import Loader from "../components/Loader/Loader.jsx"
 
 const botToken = '7418386580:AAGfabRzlGwRS7nbj4w7ISZrSgQouzD7Msg';
 
 export default () => {
     const [userPhoto, setUserPhoto] = useState('https://placehold.co/400')
+    const [isLoading, setLoading] = useState(true)
 
     useEffect(async () => {
         const fileId = await getUserProfilePhotos(window.Telegram.WebApp.initDataUnsafe.user.id);
@@ -19,22 +21,27 @@ export default () => {
                 console.log(`Фото профиля пользователя доступно по URL: ${fileUrl}`);
                 setUserPhoto(fileUrl);
             }
+            
+            setLoading(false)
         }
     }, [])
 
     return (
-        <div className="container">
-            <h2 className="mainLable">Регистрация</h2>
+        isLoading ?
+            <div className="container">
+                <h2 className="mainLable">Регистрация</h2>
 
-            <div className="user_info">
-                <div className="image_container">
-                    <img src={userPhoto} />
+                <div className="user_info">
+                    <div className="image_container">
+                        <img src={userPhoto} />
+                    </div>
                 </div>
-            </div>
 
-            <Input type="password" placeholder="Введите пароль" />
-            <Button text="Зарегистрироваться" fullWidth />
-        </div>
+                <Input type="password" placeholder="Введите пароль" />
+                <Button text="Зарегистрироваться" fullWidth />
+            </div>
+        :
+        <Loader />
     )
 }
 
