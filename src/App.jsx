99241458de2d5bf30f3fa98c43fecda5f7761af6
Dayz221 <rutuchenal37@gmail.com>
2024-09-15 10:58:05 +0300

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import './app.css'
+
 export default () => {
   const tg = window.Telegram.WebApp
 
@@ -5,7 +7,6 @@ export default () => {
     <>
       <h2>User: {tg.initDataUnsafe.user.id}</h2>
       <h2>First Name: {tg.initDataUnsafe.user.first_name}</h2>
-      <h2>Photo: {tg.initDataUnsafe.user?.photourl}</h2>
       <h2>Username: {tg.initDataUnsafe.user?.username}</h2>
     </>
   )
