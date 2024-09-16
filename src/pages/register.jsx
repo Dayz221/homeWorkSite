489f6aee3317977e2897536a856d381b97df1b77
Dayz221 <rutuchenal37@gmail.com>
2024-09-16 08:28:29 +0300

    sd

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -5,20 +5,26 @@ import '../styles/register.css'
 import axios from "axios"
 import Loader from "../components/Loader/Loader.jsx"
 
-const botToken = '7418386580:AAGfabRzlGwRS7nbj4w7ISZrSgQouzD7Msg';
+const botToken = '7418386580:AAGfabRzlGwRS7nbj4w7ISZrSgQouzD7Msg'
+const apiUrl = 'http://176.124.215.234:8000/api'
 
 export default () => {
     const [userPhoto, setUserPhoto] = useState('https://gb.ru/blog/wp-content/uploads/2022/07/gradienta-LeG68PrXA6Y-unsplash.jpg')
     const [isLoading, setLoading] = useState(true)
 
+    const [groupId, setGroupId] = useState(null)
+
     useEffect(async () => {
         const fileId = await getUserProfilePhotos(window.Telegram.WebApp.initDataUnsafe.user.id)
 
         if (fileId) {
-            const fileUrl = await getFile(fileId);
+            const fileUrl = await getFile(fileId)
             if (fileUrl) setUserPhoto(fileUrl)
-            setTimeout(() => setLoading(false), 300)
         }
+
+        const groups = await axios.get(apiUrl+'/groups')
+        console.log(groups)
+        setTimeout(() => setLoading(false), 300)
     }, [])
 
     const user = window.Telegram.WebApp.initDataUnsafe.user
@@ -36,6 +42,14 @@ export default () => {
                         <div className="user_name">{user.last_name ? user.first_name + " " + user.last_name : user.first_name}</div>
                         {user.username ? <div className="user_id">{"@" + user.username}</div> : null}
                     </div>
+                </div>
+
+                <div class="set_group">
+                    <select>
+                        <option>Option 1</option>
+                        <option>Option 2</option>
+                        <option>Option 3</option>
+                    </select>
                 </div>
 
                 <Input type="password" placeholder="Введите пароль" />
