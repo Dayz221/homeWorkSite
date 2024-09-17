108da1984e6bfb5688f1f2d165bbc49609665044
Dayz221 <rutuchenal37@gmail.com>
2024-09-17 17:45:18 +0300

    sd

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -10,12 +10,13 @@ export const Option = ({ children, key }) => {
 
 export const Select = ({ children }) => {
     const [isExpanded, setIsExpanded] = useState(false)
+    const [curElement, setCurElement] = useState(0)
     
     return (
         <div className="select_container" onClick={ () => setIsExpanded(prev => !prev) }>
-            <div className="current_element"></div>
+            <div className="current_element">dffdfdfd</div>
             <div classname={ classnames("arrow", { active: isExpanded } ) } ></div>
-            <div className="elements">{ children }</div>
+            <div className={ classnames("elements", {disable: !isExpanded}) }>{ children }</div>
         </div>
     )
 }
